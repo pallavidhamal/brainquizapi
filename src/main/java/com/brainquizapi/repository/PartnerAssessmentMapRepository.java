7594377f2d7352668532f83ec497d437b2f38e27
pallavidhamal <pallavi.dhamal@scubetechserv.com>
2022-02-21 10:48:52 +0530

    repository

--- src/main/java/com/brainquizapi/repository/PartnerAssessmentMapRepository.java
+++ src/main/java/com/brainquizapi/repository/PartnerAssessmentMapRepository.java
@@ -3,11 +3,15 @@ package com.brainquizapi.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.brainquizapi.model.PartnerAssessmentMapEntity;
 
 public interface PartnerAssessmentMapRepository extends JpaRepository<PartnerAssessmentMapEntity, Long>  {
 
 	List<PartnerAssessmentMapEntity> findAllByIsdeleted(String isdeleted);
+	
+	@Query(value = "SELECT * FROM brainquiz.partner_assessment_map where fk_assessment=?1 and fk_partner=?2", nativeQuery = true)
+	List<PartnerAssessmentMapEntity> findByPartnerAssId(String assId,String partnerId);
 
 }
